be9604e55700c4203f187b0ca2018d3a219b7d46
calvwng <calvwng@gmail.com>
2015-01-29 00:55:08 -0800

    Initial completion of Lab 4 - Weather Clock

--- js/clock.js
+++ js/clock.js
@@ -12,7 +12,41 @@ function getTime() {
    setTimeout(getTime, 1000); /* call every second */
 }
 
+function getTemp() {
+   /* AJAX request to forecast.io url */
+   $.getJSON("https://api.forecast.io/forecast/c7a60cf9130a63d5272931b9ea476aac/35.300399,-120.662362?callback=?", 
+   function(json) {
+      // Set forecast label to the daily summary
+      $("#forecastLabel").html(json.daily.summary);
+
+      // Set the icon based on the daily icon field
+      var imageSrc = "img/" + json.daily.icon + ".png";
+      $("#forecastIcon").attr("src", imageSrc);
+
+      // Set background color according to temperature
+      var curTemp = json.daily.data[0].temperatureMax;
+      var colorClass;
+      if (curTemp < 60) {
+         colorClass = "cold";
+      }
+      else if (curTemp < 70) {
+         colorClass = "chilly";
+      }
+      else if (curTemp < 80) {
+         colorClass = "nice";
+      }
+      else if (curTemp < 90) {
+         colorClass = "warm";
+      }
+      else if (curTemp >= 90) {
+         colorClass = "hot";
+      }
+      $("body").addClass(colorClass);
+   });
+}
+
 /* call getTime on page load */
 window.onload = function() {
    getTime();
+   getTemp();
 };
