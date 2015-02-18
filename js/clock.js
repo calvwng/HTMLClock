b2c31a69b5f3ee481a7c030acc1fa559261bb8b8
calvwng <calvwng@gmail.com>
2015-02-18 12:30:05 -0800

    Lab 7: testing FB login button on deployed instance

--- js/clock.js
+++ js/clock.js
@@ -267,3 +267,68 @@ function listenForTime(time) {
 
    setTimeout(listenForTime(time), 1000);     // Check every second
 }
+
+
+// This is called with the results from from FB.getLoginStatus().
+function statusChangeCallback(response) {
+ console.log('statusChangeCallback');
+ console.log(response);
+ // The response object is returned with a status field that lets the
+ // app know the current login status of the person.
+ if (response.status === 'connected') {
+   // Logged into your app and Facebook.
+   testAPI();
+ } else if (response.status === 'not_authorized') {
+   // The person is logged into Facebook, but not your app.
+   document.getElementById('status').innerHTML = 'Please log ' +
+     'into this app.';
+ } else {
+   // The person is not logged into Facebook, so we're not sure if
+   // they are logged into this app or not.
+   document.getElementById('status').innerHTML = 'Please log ' +
+     'into Facebook.';
+ }
+}
+
+// Get user's login status, and handle it through appropriate callback
+// 1. Logged into your app ('connected')
+// 2. Logged into Facebook, but not your app ('not_authorized')
+// 3. Not logged into Facebook and can't tell if they are logged into
+//    your app or not.
+function checkLoginState() {
+ FB.getLoginStatus(function(response) {
+   statusChangeCallback(response);
+ });
+}
+
+window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '352874954912281',
+    cookie     : true,  // enable cookies to allow the server to access 
+                        // the session
+    xfbml      : true,  // parse social plugins on this page
+    version    : 'v2.1' // use version 2.1
+  });
+
+  checkLoginState();
+};
+
+// Load the SDK asynchronously
+(function(d, s, id) {
+ var js, fjs = d.getElementsByTagName(s)[0];
+ if (d.getElementById(id)) return;
+ js = d.createElement(s); js.id = id;
+ js.src = "//connect.facebook.net/en_US/sdk.js";
+ fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+// Here we run a very simple test of the Graph API after login is
+// successful.  See statusChangeCallback() for when this call is made.
+function testAPI() {
+ console.log('Welcome!  Fetching your information.... ');
+ FB.api('/me', function(response) {
+   console.log('Successful login for: ' + response.name);
+   document.getElementById('status').innerHTML =
+     'Thanks for logging in, ' + response.name + '!';
+ });
+}
